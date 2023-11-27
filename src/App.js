0e3124d5256021ced248c5450badd43c9267dd29
MangCau <namanhcute2809@gmail.com>
2023-11-28 00:29:10 +0700

    na

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+//import LoginPage from "./pages/LoginPage";
 //import WelcomePage from "./pages/WelcomePage";
-import LoginPage from "./pages/LoginPage";
-import WelcomePage from "./pages/WelcomePage";
+// App.js
+import React from 'react';
+import './App.css';
+import StudentInfo from './NA/student_info';
+
 function App() {
   return (
     <div className="App">
-      <WelcomePage />
+      <StudentInfo />
     </div>
   );
 }
+
 export default App;
+
+
