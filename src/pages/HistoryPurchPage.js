f9d9b49526115eef5f4d5518d124ffafc33eb65d
Trần Đại Quý <105071667+daiquydev@users.noreply.github.com>
2023-11-29 07:06:33 +0000

    Add files via upload
    DaiQuy

---
+++ src/pages/HistoryPurchPage.js
@@ -0,0 +1,129 @@
+import React, { useState } from 'react'
+import './HistoryPurchPage.css'
+
+import { Form, Col } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Menu from '../components/Menu'
+
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap-datepicker/dist/js/bootstrap-datepicker.js'
+
+const HistoryPurchPage = () => {
+  const handleChange = (e) => {
+    // Your handling logic here
+  }
+  return (
+    <div className="PrintSchedule">
+      <Header />
+      <div className="main-content ">
+        <Menu />
+        <div className="PrintScheduleViewer">
+          <div className="content" style={{ display: 'block' }}>
+            <p
+              style={{
+                backgroundColor: '#white',
+                color: '#0346AA',
+                fontSize: '48px',
+                fontFamily: 'EB Garamond',
+                fontWeight: '700',
+                wordWrap: 'break-word',
+                padding: '10px',
+                textAlign: 'center',
+                margin: 'auto',
+              }}
+            >
+              LỊCH SỬ MUA TRANG
+            </p>
+          </div>
+          <div className="search ">
+            <div class="row g-0">
+              <div className="col-md-4" style={{ width: '291px' }}>
+                <p className="time">Thời gian</p>
+                <Form.Group as={Col} controlId="formGridName">
+                  <Form.Control
+                    type="date"
+                    placeholder="DateRange"
+                    name="bdate"
+                    style={{
+                      borderRadius: '50px',
+                      background: 'rgba(118.15, 172.89, 255, 0.5)',
+                    }}
+                    className="my-2 form-control"
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </div>
+              <div class="col-md-4" style={{ width: '291px' }}>
+                <p class="code">Mã đơn</p>
+                <input
+                  name="dserve"
+                  type="text"
+                  style={{
+                    borderRadius: '50px',
+                    background: 'rgba(118.15, 172.89, 255, 0.5)',
+                  }}
+                  class="my-2 form-control"
+                  placeholder="Nhập mã đơn hàng"
+                />
+              </div>
+              <div class="col-md-4" style={{ width: '291px' }}>
+                <p class="numPage">Số lượng trang</p>
+                <input
+                  name="dserve"
+                  type="text"
+                  style={{
+                    borderRadius: '50px',
+                    background: 'rgba(118.15, 172.89, 255, 0.5)',
+                  }}
+                  class="my-2 form-control"
+                  placeholder="Quy ra hết A4"
+                />
+              </div>
+            </div>{' '}
+          </div>{' '}
+          <div className="Table" style={{ marginTop: '46px' }}>
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col" class="col" style={{ width: '172px' }}>
+                    MÃ ĐƠN
+                  </th>
+                  <th scope="col" class="col" style={{ width: '258px' }}>
+                    THỜI GIAN
+                  </th>
+                  <th scope="col" class="col" style={{ width: '258px' }}>
+                    SỐ LƯỢNG TRANG (A4)
+                  </th>
+                  <th scope="col" class="col" style={{ width: '258px' }}>
+                    TỔNG TIỀN
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* {{#each data}}
+                                    <tr class="">
+
+                                        <td >{{this.main}}</td>
+                                        <td>{{this.mayin}}</td>
+                                        <td>{{this.time}}</td>
+                                        <td>{{this.tailieu}}</td>
+                                        <td>
+                                            <button onclick="deleteDish('{{this._id}}')"
+                                                class="btn btn-sm btn-danger" style={{color: '#FF0000', fontSize: 18, fontFamily: 'EB Garamond', fontWeight: '400', textDecoration: 'underline', wordWrap: 'break-word'}}>hủy</button>
+                                        </td>
+
+                                    </tr>
+                                    {{/each}} */}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+export default HistoryPurchPage
