ffeebc026a7748c288b5d819f9acf3e7d600670f
khoadb305 <khoa.buidang@hcmut.edu.vn>
2023-11-30 18:34:40 +0700

    no message

--- Danh sách máy in1/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { Button } from "./Button";
-export { Img } from "./Img";
-export { Input } from "./Input";
-export { ReactTable } from "./Table";
-export { Text } from "./Text";
--- Lich5su73 sspo/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { Button } from "./Button";
-export { Datepicker } from "./Datepicker";
-export { Img } from "./Img";
-export { Input } from "./Input";
-export { ReactTable } from "./Table";
-export { Text } from "./Text";
