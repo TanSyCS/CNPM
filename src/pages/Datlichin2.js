7a07b28a5d2049a6da77d3a1b775ae84d557d07f
MangCau <namanhcute2809@gmail.com>
2023-12-07 10:18:56 +0700

    fix fe

--- src/pages/Datlichin2.js
+++ src/pages/Datlichin2.js
@@ -1,166 +1,106 @@
 import React from 'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Logo from '../components/Logo'
+
 import Menu from '../components/Menu'
-import { Form, Col } from 'react-bootstrap'
+
 import './Datlichin2.css'
-function Datlichin2()
-{
-        return (
-                
-                <div className="PrintSchedule">
+const Datlichin2 = () => {
+    const ConfirmSetPrint = () => {
+        alert("Xác nhận đặt lịch in?");
+    };
 
-                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
+    const CancelSetPrint = () => {
+        alert("Hủy đặt lịch in?");
+    };
+    const handleFileChange = (event) => {
+        const selectedFile = event.target.files[0];
+        // Handle the selected file as needed, you can store it in state or perform other actions
+        console.log("Selected file:", selectedFile);
+    };
+    const addressOptions = ["Address 1", "Address 2", "Address 3"];
+    const printerOptions = ["Printer 1", "Printer 2", "Printer 3"];
+    const sizeOptions = ["A3", "A4"];
+    return (
 
-                        <Header />
-                        <div className="main-content ">
-                                <Menu />
-                                <div className="PrintScheduleViewer">
-                                                                <div className="content">
-                                                                    <p
-                                                                        style={{
-                                                                                backgroundColor: '#white',
-                                                                                color: '#0346AA',
-                                                                                fontSize: '68px',
-                                                                                fontFamily: 'EB Garamond',
-                                                                                fontWeight: '700',
-                                                                                wordWrap: 'break-word',
-                                                                                padding: '10px',
-                                                                                textAlign: 'center',
-                                                                                margin: 'auto',
-                                                                               
-                                                                        }}
-                                                                    > Đặt lịch in </p>
-                                                                    <p
-                                                                        style={{
-                                                                                backgroundColor: '#white',
-                                                                                color: '#0346AA',
-                                                                                fontSize: '18px',
-                                                                                fontFamily: 'EB Garamond',
-                                                                                fontWeight: '700',
-                                                                                wordWrap: 'break-word',
-                                                                                padding: '20px',
-                                                                                textAlign: 'center',
-                                                                                margin: 'auto',
-                                                                               
-                                                                        }}
-                                                                    >Thông tin lịch in</p>
-                                                                </div>
-                                                                <div className="siu">
-                                                                    <table border="1" ceilpadding="8px" cellspacing="7px" align="center" bgcolor="lightblue">
-                                                                    <tr bgcolor="lightgreen">
-                                                                            <th>
-                                                                                Sinh Viên
-                                                                            </th>
-                                                                            <th>
-                                                                                Trần Nguyễn Nam Anh
-                                                                            </th>
-                                                                    </tr> 
-                                                                        <tr>
-                                                                                <th>
-                                                                                    Mã số sinh viên 
-                                                                                </th>
-                                                                                <th>
-                                                                                        2110758
-                                                                                </th>
-                                                                        </tr>
-                                                                    <tr>
-                                                                            <th>
-                                                                                Thời gian
-                                                                            </th>
-                                                                            <th>
-                                                                                Thứ 4 (29/11/2023), ca chiều
-                                                                            </th>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <th>
-                                                                            Tải tài liệu
-                                                                        </th>
-                                                                        <th>
-                                                                            <label for="file">File</label>
-                                                                            <input id="file" type="file" name="file"></input>
-                                                                        </th>
-                                                                    </tr>
-                                                                    <tr>
-                                                                            <th>
-                                                                                Địa điểm
-                                                                            </th>
-                                                                            <th>
-                                                                                <input type="text" name="name" id="name" placeholder="Địa điểm" required></input>
-                                                                            </th>
-                                                                        </tr>
-                                                                    <tr>
-                                                                        <th>
-                                                                            Máy in
-                                                                        </th>
-                                                                        <th>
-                                                                            <input type="text" name="name" id="name" placeholder="Mã máy in" required></input>
-                                                                        </th>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <th>
-                                                                            Kích thước trang
-                                                                        </th>
-                                                                        <th>
-                                                                        <select name="size" id="size" multiple>
-                                                                            <option value="Large">Green</option>
-                                                                            <option value="Medium">Red</option>
-                                                                            <option value="Small">Red</option>
-                                                                        </select>
-                                                                        </th>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <th>
-                                                                            Số lượng trang
-                                                                        </th>
-                                                                        <th>
-                                                                               <label for="age"></label>
-                                            `                                  <input type="number" name="age" id="age" min="1" max="200" step="2" placeholder="Nhập số lượng trang" ></input>
-                                                                        </th>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <th>
-                                                                            Số mặt in
-                                                                        </th>
-                                                                        <th>
-                                                                        <div>
-                                                                    <label for="male">1</label>
-                                                                    <input type="radio" name="gender" id="one" value="one"></input>
-                                                                </div>
-                                                                <div>
-                                                                    <label for="female">2</label>
-                                                                    <input type="radio" name="gender" id="two" value="two"></input>
-                                                                </div>
-                                                                        </th>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <th>
-                                                                            Số bản in
-                                                                        </th>
-                                                                        <th>
-                                                                               <label for="age"></label>
-                                            `                                  <input type="number" name="age" id="age" min="1" max="200" step="2" placeholder="Nhập số lượng trang" ></input>
-                                                                        </th>
-                                                                    </tr>
-                                                                    
-                                                                    
-                                                                    </table>
-                                                                </div>
-                                                        <div className="but">        
-                                                                <button class="custom-button">Đặt lịch in</button>
-                                                                <button class="custom-button">Hủy</button>
-                                                        </div>
-                                                        <div className="move">
-                                                            <a href="http://www.imdb.com">TIẾP THEO</a>
-                                                        </div>
-
-                                                        
-
-                                </div>
-                        </div>
-                        <Footer />
+        <div className="PrintSchedule">
+            <Header />
+            <div className="main-contentBBA ">
+                <Menu />
+                <div className="PrintScheduleViewerBAB">
+                    <p id="p111">ĐẶT LỊCH IN</p>
+                    <p id="p211">Thông tin lịch in</p>
+                    <table className="info-tableNa13">
+                        <tbody>
+                            <tr>
+                                <td className="table-contentNa13"><label htmlFor="namesv">Sinh viên</label></td>
+                                <td>Trần Nguyễn Nam Anh</td>
+                            </tr>
+                            <tr>
+                                <td className="table-contentNa13"><label htmlFor="idsv">Mã số sinh viên</label></td>
+                                <td>2110758</td>
+                            </tr>
+                            <tr>
+                                <td className="table-contentNa13"><label htmlFor="khoa">Thời gian</label></td>
+                                <td>Thứ 4, 29/11/2023, ca chiều</td>
+                            </tr>
+                            <tr>
+                                <td className="table-contentNa13"><label htmlFor="upfile">Tải tài liệu</label></td>
+                                <td><input type="file" id="upfile" name="upfile" onChange={handleFileChange} accept=".pdf, .doc, .docx" /></td>
+                            </tr>
+                            <tr>
+                                <td className="table-contentNa13"><label htmlFor="addr">Địa điểm</label></td>
+                                <td><select id="addr" name="addr">
+                                    {addressOptions.map((option) => (
+                                        <option key={option} value={option}>{option}</option>
+                                    ))}
+                                </select></td>
+                            </tr>
+                            <tr>
+                                <td className="table-contentNa13"><label htmlFor="printer">Máy in</label></td>
+                                <td>
+                                    <select id="printer" name="printer">
+                                        {printerOptions.map((option) => (
+                                            <option key={option} value={option}>{option}</option>
+                                        ))}
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="table-contentNa13"><label htmlFor="size">Kích thước trang</label></td>
+                                <td>
+                                    <select id="size" name="size">
+                                        {sizeOptions.map((option) => (
+                                            <option key={option} value={option}>{option}</option>
+                                        ))}
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="table-contentNa13"><label htmlFor="numpage">Số lượng trang</label></td>
+                                <td><input type="numpage" id="numpage" name="numpage"></input></td>
+                            </tr>
+                            <tr>
+                                <td className="table-contentNa13"><label htmlFor="ful-semi">Số mặt in</label></td>
+                                <td><input type="number" id="ful-semi" name="ful-semi"></input></td>
+                            </tr>
+                            <tr>
+                                <td className="table-contentNa13"><label htmlFor="times">Số bản in</label></td>
+                                <td><input type="number" id="times" name="times"></input></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br></br><br></br>
+                    <div className="action123">
+                        <button style={{ backgroundColor: "#76ADFF" }} onClick={ConfirmSetPrint}>XÁC NHẬN</button>
+                        <button style={{ backgroundColor: "#EB4A4A" }} onClick={CancelSetPrint}>HỦY</button>
+                    </div>
                 </div>
-        ) 
+
+
+            </div>
+            <Footer />
+        </div>
+    )
 };
 export default Datlichin2;
